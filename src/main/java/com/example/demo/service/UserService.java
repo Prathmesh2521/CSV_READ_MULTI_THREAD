20f108eec1913df0f881289e84cc4f56e630663c
yadavpranjali <yadavpranjali6598@gmail.com>
2024-07-20 20:29:38 +0530

    Initial check in

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,73 @@
+package com.example.demo.service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	Object target;
+	Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Async
+	public CompletableFuture<List<User>> saveUser(MultipartFile file) throws Exception
+	{
+		long start = System.currentTimeMillis();
+		List<User> users = parseCsv(file);
+		logger.info("Saving list of users of size "+users.size()+" "+Thread.currentThread().getName());
+		users = userRepository.saveAll(users);
+		long end = System.currentTimeMillis();
+		logger.info("Total time taken " + (end - start));
+		return CompletableFuture.completedFuture(users);
+	}
+	
+	@Async
+	public CompletableFuture<List<User>> findAllUsers()
+	{
+		logger.info("Get list of users "+ Thread.currentThread().getName());
+		List<User> users = userRepository.findAll();
+		return CompletableFuture.completedFuture(users);
+	}
+	
+	
+	private List<User> parseCsv(final MultipartFile file) throws Exception
+	{
+		final List<User> users = new ArrayList<>();
+		try {
+			try(final BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream())))
+			{
+				String line;
+				while((line = br.readLine()) != null)
+				{
+					final String[] data = line.split(",");
+					final User user = new User();
+					user.setName(data[0]);
+					user.setEmail(data[1]);
+					user.setGender(data[2]);
+					users.add(user);
+				}
+				return users;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.error("Failed to parse CSV", e);
+			throw new Exception("Failed to parse CSV", e);
+		}
+	}
+}
